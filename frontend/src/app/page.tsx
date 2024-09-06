ca33d50536f28e9ebe2125d20786f66baaa96705
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2024-09-06 20:10:39 +0530

    initital commit

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,17 @@
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-50">
+      <h1 className="text-4xl font-bold mb-8">Welcome to Our App</h1>
+      <div className="space-x-4">
+        {/* <Link href="/login" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Login
+        </Link> */}
+        <Link href="/signup" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
+}
